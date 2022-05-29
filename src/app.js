a2fe2a809cb13e601c360bd6eaa8b7fcd90f4506
trance <michaelmunavu83@gmail.com>
2022-05-29 15:39:17 +0300

    Add queue

---
+++ src/app.js
@@ -0,0 +1,11 @@
+export default class Book {
+  constructor(author, title) {
+    this.author = author;
+    this.title = title;
+  }
+
+  static addauth() {
+    document.getElementById('auth').innerHTML = 'bnm,';
+    console.log('mm8hnm,');
+  }
+}
